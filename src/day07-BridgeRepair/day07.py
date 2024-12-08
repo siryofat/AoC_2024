7a9e7ae923baf2b56a5f6d3938c226bbeb5d6003
SirYofat <siryofat@gmail.com>
2024-12-09 00:57:55 +0100

    feat: testing day07

--- src/day07-BridgeRepair/day07.py
+++ src/day07-BridgeRepair/day07.py
@@ -6,46 +6,48 @@ def is_in_range(value:int, numbers:list[int]) -> bool:
     max_value = math.prod(numbers)
     return min_value <= value <= max_value
 
-def get_permutation(length: int):
+def get_permutation(length: int) -> list:
     operands = ['+', '*']
-    return itertools.product(operands, repeat=length)
+    # permutations = list(itertools.product(operands, repeat=length))
+    permutations = list(itertools.permutations(operands*length, length))
+    return permutations
 
 def read_file(file_path):
     with open(file_path, 'r') as file:
         for line in file:
-            yield line.strip()
+            yield line
 
 def test_line(line):
-    try:
-        result, operation = line.split(':')
-        result = int(result)
-        nums = [int(num) for num in operation.split()]
-        if not is_in_range(result, nums):
-            return 0
-
-        for permutation in get_permutation(len(nums)-1):
+    result, operation = line.split(':')
+    result = int(result)
+    nums = [int(num) for num in operation.split()]
+    test_range = is_in_range(result, nums)
+    if test_range:
+        permutations = get_permutation(len(nums)-1)
+        permutations = set(permutations)
+        for permutation in permutations:
             num1 = nums[0]
             for num2, operand in zip(nums[1:], permutation):
-                num1 = eval(f"{num1}{operand}{num2}")
+                to_eval = [num1, operand, num2]
+                to_eval = ''.join(str(item) for item in to_eval)
+                num1 = eval(to_eval)
             if num1 == result:
-                return result
-        return 0
-    except Exception as e:
-        print(f"Error processing line: {line}")
-        print(f"Error details: {e}")
-        return 0
+                del result, operation, nums, test_range, permutations
+                return num1
+    del result, operation, nums, test_range
+    return 0
 
 def main():
     total = 0
     for i, line in enumerate(read_file('src/day07-BridgeRepair/day07.txt')):
-        print(f"Processing line {i}: {line}")
-        result = test_line(line)
-        total += result
-        print(f"Result: {result}, Running Total: {total}")
+        print(i)
+        if i < 100:
+            result = test_line(line)
+            total += result
+    print(total)
 
-    print(f"Final Total: {total}")
-
-    # Test the problematic line separately
+    print(72095400778)
+    tl = [1, 7, 4, 1, 5, 6, 6, 79, 3, 5, 2, 780]
     print(test_line('72095400778: 1 7 4 1 5 6 6 79 3 5 2 780'))
 
 main()
