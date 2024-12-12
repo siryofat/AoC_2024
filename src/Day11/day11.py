b99a003118bef38e42516df63635da3c5bd48b32
SirYofat <siryofat@gmail.com>
2024-12-12 23:42:35 +0100

    test: try with recursive

--- src/Day11/day11.py
+++ src/Day11/day11.py
@@ -1,6 +1,6 @@
-#test_string, blinks = '125 17', 6 #22
+test_string, blinks = '125 17', 6 #22
 #test_string, blinks = '3 386358 86195 85 1267 3752457 0 741', 25 #183248
-test_string, blinks = '3 386358 86195 85 1267 3752457 0 741', 75 #183248
+#test_string, blinks = '3 386358 86195 85 1267 3752457 0 741', 75 #183248
 #wrongs = [78]
 
 def clean(item:str) -> str:
@@ -29,20 +29,27 @@ def arranger(input_list:list) -> list:
 
 test_list = [item for item in test_string.split()]
 
+def longer(input_list:list, stop):
+    for stone in input_list:
+        if stop < 0:
+            return 0
+        stop -= 1
+        if stone == '0':
+            return 1+longer(['1'],stop)
+        if len(stone)%2 == 0:
+            half = len(stone)//2
+            first_half = stone[:half]
+            second_half = stone[half:]
+            return 2+longer([first_half, second_half],stop)
+        last = int(stone)*2024
+        return 1+longer([str(last)],stop)
+
+#print(list(longer(test_list, blinks)))
+print(longer(test_list, blinks))
+
 # for i in range(blinks):
 #     print(f'iteration {i}, list before function {test_list=}')
 #     test_list = arranger(test_list)
 #     print(f'list after function {test_list=}\n')
 
 # print(len(test_list))
-
-total = 0
-for stone in test_list:
-    run = [stone]
-    print(f'running for {stone}:')
-    for i in range(blinks):
-        run = arranger(run)
-    print(f'this run {len(run)}')
-    total += len(run)
-
-print(total)
