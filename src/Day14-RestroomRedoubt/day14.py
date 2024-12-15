cadaaf4192ad4af97afed95e07ffaf67659632ef
SirYofat <siryofat@gmail.com>
2024-12-15 22:16:57 +0100

    feat: adds move_robot function

--- src/Day14-RestroomRedoubt/day14.py
+++ src/Day14-RestroomRedoubt/day14.py
@@ -1,6 +1,4 @@
-test = 'p=0,4 v=3,-3'
-
-p, v = test.split()
+test = 'p=2,4 v=2,-3'
 
 def read_line_values(line_info:str) -> tuple:
     '''
@@ -12,3 +10,25 @@ def read_line_values(line_info:str) -> tuple:
     vel_x, vel_y = map(int, velocity.split('=')[1].split(','))
     return (pos_x, pos_y), (vel_x, vel_y)
 
+def move_robot(initial_position: tuple,
+               velocity: tuple,
+               time: int,
+               space: tuple,) -> tuple:
+    x, y = initial_position
+    vx, vy = velocity
+    x_max, y_max = space
+
+    new_x = x + time * vx
+    new_y = y + time * vy
+
+    # Since it's goint to teleport, its enough to remove extra space
+    # from final positions:
+    extra_x = new_x // (x_max)
+    extra_y = new_y // (y_max)
+    corrected_x = new_x - extra_x * x_max
+    corrected_y = new_y - extra_y * y_max
+
+    return corrected_x, corrected_y
+
+p, v = read_line_values(test)
+x, y = move_robot(p,v,5,(11,7))
